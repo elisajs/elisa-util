2fdee235e757f87686f7094c80600d83fbf4410d
Raúl G. González <raulggonzalez@justojs.org>
2016-04-19 13:10:19 +0200

    Fix #6, fix #7

--- dist/es5/nodejs/elisa-util/lib/Projector.js
+++ dist/es5/nodejs/elisa-util/lib/Projector.js
@@ -14,35 +14,59 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
     docs, fields) {var opts = arguments.length <= 2 || arguments[2] === undefined ? {} : arguments[2];
-      var rows;
+      var res;
+
+
+      if (!fields) fields = {};
 
 
       if (Object.keys(fields).length === 0) {
-        rows = docs;} else 
+        res = docs;} else 
       {
-        rows = [];var _iteratorNormalCompletion = true;var _didIteratorError = false;var _iteratorError = undefined;try {
-
-          for (var _iterator = docs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {var doc = _step.value;
-            var row = {};
-
-            if (typeof fields == "string") {
-              if (opts.top) row = doc[fields];else 
-              prj(doc, fields, fields, row);} else 
-            if (fields instanceof Array) {var _iteratorNormalCompletion2 = true;var _didIteratorError2 = false;var _iteratorError2 = undefined;try {
-                for (var _iterator2 = fields[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {var field = _step2.value;prj(doc, field, field, row);}} catch (err) {_didIteratorError2 = true;_iteratorError2 = err;} finally {try {if (!_iteratorNormalCompletion2 && _iterator2.return) {_iterator2.return();}} finally {if (_didIteratorError2) {throw _iteratorError2;}}}} else 
-            {
-              for (var _field in fields) {prj(doc, _field, fields[_field], row);}}
-
-
-            rows.push(row);}} catch (err) {_didIteratorError = true;_iteratorError = err;} finally {try {if (!_iteratorNormalCompletion && _iterator.return) {_iterator.return();}} finally {if (_didIteratorError) {throw _iteratorError;}}}}
+        if (docs instanceof Array) {
+          res = [];var _iteratorNormalCompletion = true;var _didIteratorError = false;var _iteratorError = undefined;try {
+            for (var _iterator = docs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {var doc = _step.value;res.push(prjDoc(doc, fields, opts));}} catch (err) {_didIteratorError = true;_iteratorError = err;} finally {try {if (!_iteratorNormalCompletion && _iterator.return) {_iterator.return();}} finally {if (_didIteratorError) {throw _iteratorError;}}}} else 
+        {
+          res = prjDoc(docs, fields, opts);}}
 
 
 
 
-      return rows;} }]);return Projector;}();exports.default = Projector;
+      return res;} }]);return Projector;}();exports.default = Projector;
 
 
 
-function prj(doc, name, alias, row) {
+function prjDoc(doc, fields, opts) {
+  var res;
+
+
+  res = {};
+
+  if (typeof fields == "string") {
+    if (opts.top) res = doc[fields];else 
+    prjField(doc, fields, fields, res);} else 
+  if (fields instanceof Array) {var _iteratorNormalCompletion2 = true;var _didIteratorError2 = false;var _iteratorError2 = undefined;try {
+      for (var _iterator2 = fields[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {var field = _step2.value;prjField(doc, field, field, res);}} catch (err) {_didIteratorError2 = true;_iteratorError2 = err;} finally {try {if (!_iteratorNormalCompletion2 && _iterator2.return) {_iterator2.return();}} finally {if (_didIteratorError2) {throw _iteratorError2;}}}} else 
+  {
+    for (var _field in fields) {prjField(doc, _field, fields[_field], res);}}
+
+
+
+  return res;}
+
+
+function prjField(doc, name, alias, row) {
   if (doc.hasOwnProperty(name)) row[alias] = doc[name];}
--- test/unit/lib/Projector.js
+++ test/unit/lib/Projector.js
@@ -8,63 +8,99 @@ const Projector = require("../../../dist/es5/nodejs/elisa-util").Projector;
 
 //suite
 suite("Projector", function() {
-  const docs = [
-    {x: 1, y: 1, z: 1},
-    {x: 1, y: 2, z: 2},
-    {x: 2, y: 2, z: 2},
-    {x: 2, y: 3, z: 3},
-    {x: 3, y: 3, z: 3},
-    {x: 3, y: 4, z: 4}
-  ];
   const project = new Projector().project;
 
+
   suite("#project()", function() {
-    test("project(docs, {})", function() {
-      project(docs, {}).must.be.eq(docs);
+    suite("project(object, ...)", function() {
+      const doc = {x: 111, y: 222, z: 333};
+
+      test("project(doc, undefined)", function() {
+        project(doc, undefined).must.be.eq(doc);
+      });
+
+      test("project(doc, {})", function() {
+        project(doc, {}).must.be.eq(doc);
+      });
+
+      test("project(doc, field)", function() {
+        project(doc, "x").must.be.eq({x: 111});
+      });
+
+      test("project(doc, field, {top})", function() {
+        project(doc, "x", {top: true}).must.be.eq(111);
+      });
+
+      test("project(doc, fields : string[])", function() {
+        project(doc, ["x", "z"]).must.be.eq({x: 111, z: 333});
+      });
+
+      test("project(doc, fields : object)", function() {
+        project(doc, {x: "a", z: "b"}).must.be.eq({a: 111, b: 333});
+      });
     });
 
-    test("project(docs, field)", function() {
-      project(docs, "x").must.be.eq([
-        {x: 1},
-        {x: 1},
-        {x: 2},
-        {x: 2},
-        {x: 3},
-        {x: 3}
-      ]);
-    });
+    suite("project(object[], ...)", function() {
+      const docs = [
+        {x: 1, y: 1, z: 1},
+        {x: 1, y: 2, z: 2},
+        {x: 2, y: 2, z: 2},
+        {x: 2, y: 3, z: 3},
+        {x: 3, y: 3, z: 3},
+        {x: 3, y: 4, z: 4}
+      ];
 
-    test("project(docs, field, {top})", function() {
-      project(docs, "x", {top: true}).must.be.eq([
-        1,
-        1,
-        2,
-        2,
-        3,
-        3
-      ]);
-    });
+      test("project(docs, undefined)", function() {
+        project(docs, undefined).must.be.eq(docs);
+      });
 
-    test("project(docs, fields : array)", function() {
-      project(docs, ["x", "y"]).must.be.eq([
-        {x: 1, y: 1},
-        {x: 1, y: 2},
-        {x: 2, y: 2},
-        {x: 2, y: 3},
-        {x: 3, y: 3},
-        {x: 3, y: 4}
-      ]);
-    });
+      test("project(docs, {})", function() {
+        project(docs, {}).must.be.eq(docs);
+      });
 
-    test("project(docs, fields : object)", function() {
-      project(docs, {x: "a", y: "b"}).must.be.eq([
-        {a: 1, b: 1},
-        {a: 1, b: 2},
-        {a: 2, b: 2},
-        {a: 2, b: 3},
-        {a: 3, b: 3},
-        {a: 3, b: 4}
-      ]);
+      test("project(docs, field)", function() {
+        project(docs, "x").must.be.eq([
+          {x: 1},
+          {x: 1},
+          {x: 2},
+          {x: 2},
+          {x: 3},
+          {x: 3}
+        ]);
+      });
+
+      test("project(docs, field, {top})", function() {
+        project(docs, "x", {top: true}).must.be.eq([
+          1,
+          1,
+          2,
+          2,
+          3,
+          3
+        ]);
+      });
+
+      test("project(docs, fields : string[])", function() {
+        project(docs, ["x", "y"]).must.be.eq([
+          {x: 1, y: 1},
+          {x: 1, y: 2},
+          {x: 2, y: 2},
+          {x: 2, y: 3},
+          {x: 3, y: 3},
+          {x: 3, y: 4}
+        ]);
+      });
+
+      test("project(docs, fields : object)", function() {
+        project(docs, {x: "a", y: "b"}).must.be.eq([
+          {a: 1, b: 1},
+          {a: 1, b: 2},
+          {a: 2, b: 2},
+          {a: 2, b: 3},
+          {a: 3, b: 3},
+          {a: 3, b: 4}
+        ]);
+      });
     });
   });
 })();
